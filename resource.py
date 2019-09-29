fa07a2bde242b49825cb3cfe652c21871a529e67
Brahma Reddy Seelam <bseelam@IM-LP-585.local>
2019-09-29 19:06:28 +0530

    introduced the function

--- resource.py
+++ resource.py
@@ -1,28 +1,43 @@
 import boto3
-session = boto3.Session()
-ec2_re = session.resource(service_name="ec2")
-for i in ec2_re.instances.all():
-    print(i.public_dns_name,i.id,i.state["Name"],i.image_id)
+def get_ec2_list():
+   # type: () -> object
+   session = boto3.Session()
+   ec2_re     = boto3.resource(service_name="ec2")
+   for i in ec2_re.instances.all():
+       print(i.public_dns_name, i.id, i.state["Name"], i.image_id)
+   return
 
-ec2 = boto3.resource('ec2')
+#session = boto3.Session()
+#ec2_re = session.resource(service_name="ec2")
 
-print("Trying to find Running Instances")
-instances = ec2.instances.filter(
-    Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+instance_list=get_ec2_list()
 
-for instance in instances:
-    print(instance.id, instance.instance_type)
 
-print("Trying to find Stopped Instances")
-instances = ec2.instances.filter(
-    Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}])
 
-for instance in instances:
-    print(instance.id, instance.instance_type)
+#for i in ec2_re.instances.all():
+ #   print(i.public_dns_name,i.id,i.state["Name"],i.image_id)
 
-print("Trying to find Terminated Instances")
-instances = ec2.instances.filter(
-    Filters=[{'Name': 'instance-state-name', 'Values': ['terminated']}])
+#ec2 = boto3.resource('ec2')
 
-for instance in instances:
-    print(instance.id, instance.instance_type)
+#print("Trying to find Running Instances")
+#instances = ec2.instances.filter(
+ #   Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+
+#for instance in instances:
+ #   print(instance.id, instance.instance_type)
+
+#print("Trying to find Stopped Instances")
+#instances = ec2.instances.filter(
+ #   Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}])
+
+#for instance in instances:
+#    print(instance.id, instance.instance_type)
+
+#print("Trying to find Terminated Instances")
+#instances = ec2.instances.filter(
+ #   Filters=[{'Name': 'instance-state-name', 'Values': ['terminated']}])
+
+#for instance in instances:
+#    print(instance.id, instance.instance_type)
+
+
